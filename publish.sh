779d5af4f293c835e97ab53d6cc9f48ebc4af14d
aball <aball-NONEMP@Collegeboard.org>
2019-07-14 10:25:06 -0400

    updated version and publish.sh

--- publish.sh
+++ publish.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 #To publish, run this file to ensure all proper steps are taken first
+
+npm run test
+
+if [ $? -ne 0 ]; then
+  echo "Error Unit Test(s) Failed!"
+  exit 1
+else
+  echo "All tests passed."
+fi
+
+#Get all the outdated npm modules
+outdatedModules=$(npm outdated)
+
+#Check to see if any of the outdated modules contains create-react-app
+regex="create-react-app"
+
+if [[ $outdatedModules =~ $regex ]];
+then 
+  echo "create-react-app is out of date, please update before committing latest version of rgc."
+else 
+  npm publish
+  echo "Successfully publish to npm!"
+fi;
